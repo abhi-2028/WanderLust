da812f7e999190ffb9ac497070eb4073d73db1c7
Abhirup Biswas <abhirupbiswas1clg@gmail.com>
2025-10-15 23:01:57 +0530

    added delete route and basic style setup

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Listing = require("./models/listing");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -23,6 +24,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname,"public")));
 
 app.get("/", (req,res) => {
     res.send('this is root');
@@ -65,6 +68,14 @@ app.get("/listings/:id/edit", async (req,res) => {
 app.put("/listings/:id", async(req,res) => {
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect("/listings");
+})
+
+//Delete Route
+app.delete("/listings/:id", async(req,res) => {
+    let {id} = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
     res.redirect("/listings");
 })
 

97009ba31257794a77e0e6836652a0b24a9116a6
Abhirup Biswas <abhirupbiswas1clg@gmail.com>
2025-10-15 11:21:51 +0530

    added the edit and update route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const Listing = require("./models/listing");
 const path = require("path");
+const methodOverride = require("method-override");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -21,6 +22,7 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req,res) => {
     res.send('this is root');
@@ -51,6 +53,20 @@ app.post("/listings", async (req, res) => {
     res.redirect("/listings");
   
 });
+
+//Edit Route
+app.get("/listings/:id/edit", async (req,res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("./listings/edit.ejs", {listing});
+});
+
+//Update Route
+app.put("/listings/:id", async(req,res) => {
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect("/listings");
+})
 
 app.listen(8080, () => {
     console.log('Server is listening to port 8080');

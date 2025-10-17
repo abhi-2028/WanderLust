112b4070fb8730c1e2f454b282256d6164d90608
Abhirup Biswas <abhirupbiswas1clg@gmail.com>
2025-10-17 22:44:46 +0530

    handling client side validation

--- models/listing.js
+++ models/listing.js
@@ -6,7 +6,10 @@ const listingSchema = new Schema({
     type: String,
     required: true,
   },
-  description: String,
+  description:{
+    type: String,
+    required: true,
+  },
   image: {
     type: String,
     default:
@@ -16,9 +19,19 @@ const listingSchema = new Schema({
         ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
         : v,
   },
-  price: Number,
-  location: String,
-  country: String,
+  price: {
+    type: Number,
+    required: true,
+    min: [1,"Price must be greater than 0"],
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  country:{
+    type: String,
+    required: true,
+  },
 });
 
 const Listing = mongoose.model("Listing", listingSchema);

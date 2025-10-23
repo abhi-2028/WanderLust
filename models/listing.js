7549faf43d7ab3d98a5062842e72586b48c4267e
Abhirup Biswas <abhirupbiswas1clg@gmail.com>
2025-10-23 17:15:26 +0530

    Integrated cloudinary storage for cloud image storage

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Review = require("./review");
+const { fileLoader } = require("ejs");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
@@ -12,13 +13,8 @@ const listingSchema = new Schema({
     required: true,
   },
   image: {
-    type: String,
-    default:
-      "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
-    set: (v) =>
-      v === ""
-        ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
-        : v,
+    url: String,
+    filename: String,
   },
   price: {
     type: Number,
--- routes/listing.js
+++ routes/listing.js
@@ -3,13 +3,16 @@ const router = express.Router();
 const Listing = require("../models/listing");
 const wrapAsync = require("../utils/wrapAsync");
 const { isLoggedIn, isOwner, validateListing } = require('../utils/middleware');
-
 const listingController = require('../controllers/listings');
+const multer  = require('multer');
+const {storage} = require('../cloudConfig');
+const upload = multer({ storage });
 
 router.route("/")
     .get(wrapAsync(listingController.index))
     .post(
         isLoggedIn,
+        upload.single('listing[image]'),
         validateListing,
         wrapAsync(listingController.createListing)
     );

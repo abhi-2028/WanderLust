40828647141ec092f6b8a29c3b2fb56f417cd562
Abhirup Biswas <abhirupbiswas1clg@gmail.com>
2025-10-23 18:32:29 +0530

    restructure project files into MVC pattern and move middleware/utils

--- routes/listing.js
+++ routes/listing.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const Listing = require("../models/listing");
 const wrapAsync = require("../utils/wrapAsync");
-const { isLoggedIn, isOwner, validateListing } = require('../utils/middleware');
+const { isLoggedIn, isOwner, validateListing } = require('../middleware/middleware');
 const listingController = require('../controllers/listings');
 const multer  = require('multer');
-const {storage} = require('../cloudConfig');
+const {storage} = require('../utils/cloudConfig');
 const upload = multer({ storage });
 
 router.route("/")
